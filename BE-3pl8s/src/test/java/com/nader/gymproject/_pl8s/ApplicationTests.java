568c8071ab448a1ecdcf55574e941480983d25b0
Nader Awadala <naderawadala@gmail.com>
2024-07-26 16:52:53 +0300

    setup be

---
+++ BE-3pl8s/src/test/java/com/nader/gymproject/_pl8s/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nader.gymproject._pl8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
